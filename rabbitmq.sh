f082345bea7884319a8fc47844f8ff351cc47224
tharun <tharundadithota@gmail.com>
2025-06-27 22:41:25 +0530

    user and cart setup

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,43 +1,11 @@
 #!/bin/bash
 
-# At 1st we need the root access for all --> so we need to check the root access 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-# creating a variable here
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD # gives the pwd where the script is present
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE 
-
-# Check the user has root privileges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #if you want to manually exit then give other than 0, upto 127
-else
-    echo "You are running with the root access" | tee -a $LOG_FILE
-fi
+source ./common.sh
+app_name=rabbitmq
+check_root
 
 echo "Please enter rabbitmq password to setup"
 read -s RABBITMQ_PASSWD
-
-# validate function takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE  # tee command --> adds single input to the multiple outputs to the screen and also to the file.
-    else 
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE 
-        exit 1
-    fi
-}
 
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding rabbitmq repo"
@@ -55,7 +23,4 @@ VALIDATE $? "starting rabbitmq server"
 rabbitmqctl add_user roboshop $RABBITMQ_PASSWD &>>$LOG_FILE # password should be roboshop123, because it has been already configured the same in the payment and dispatch code too.
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
